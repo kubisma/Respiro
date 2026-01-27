dfaaa40f43b4ebc6502b8ada3450ea8cd5359eee
kubisma <213828029+kubisma@users.noreply.github.com>
2026-01-27 22:15:23 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,38 +1,37 @@
 const CACHE_NAME = 'respiro-v1';
 
 const APP_ASSETS = [
-  './',
-  './index.html',
-  './manifest.webmanifest',
-  './favicon.ico',
+  './',                     
+  'index.html',            
+  'manifest.webmanifest',
+  'favicon.ico',
 
-  './css/style.css',
-  './fonts/lato-regular.woff2',
+  'css/style.css',
+  'fonts/lato-regular.woff2',
 
-  './src/app.js',
-  './src/data.js',
-  './src/dom.js',
-  './src/features.js',
-  './src/offline.js',
-  './src/session.js',
-  './src/sound.js',
-  './src/state.js',
-  './src/texts.js',
-  './src/theme.js',
-  './src/ui.js',
+  'src/app.js',
+  'src/data.js',
+  'src/dom.js',
+  'src/features.js',
+  'src/offline.js',
+  'src/session.js',
+  'src/sound.js',
+  'src/state.js',
+  'src/texts.js',
+  'src/theme.js',
+  'src/ui.js',
 
-  './sounds/inhale.mp3',
-  './sounds/hold.mp3',
-  './sounds/exhale.mp3',
+  'sounds/inhale.mp3',
+  'sounds/hold.mp3',
+  'sounds/exhale.mp3',
 
-  './icons/icon-192.png',
-  './icons/icon-512.png'
+  'icons/icon-192.png',
+  'icons/icon-512.png'
 ];
-
 
 // Instalacja
 self.addEventListener('install', event => {
-  self.skipWaiting();
+  self.skipWaiting();    
   event.waitUntil(
     caches.open(CACHE_NAME).then(cache => {
       return cache.addAll(APP_ASSETS);
